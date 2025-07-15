e8bb85fbd43b243f55325eadeefb5b1fcee28eb3
manoj <manoj@intern.schmidvision.com>
2025-07-15 17:18:57 +0530

    after connect with  backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { messaging, getToken, onMessage } from "./firebase-config";
+import axios from "axios";
 
 function App() {
   const [fcmToken, setFcmToken] = useState("");
@@ -20,18 +21,33 @@ function App() {
         }).catch(err => console.log("Token Error", err));
       }
     });
-
+   
+    
     onMessage(messaging, (payload) => {
       console.log("Message Received", payload);
       alert(`${payload.notification.title}\n${payload.notification.body}`);
     });
   }, []);
-
+  const sendPushNotification = async () => {
+    console.log(fcmToken)
+    try {
+      const response = await axios.post('http://localhost:4000/notification', {
+        token: fcmToken
+      });
+      if (response.status === 200) {
+        console.log("✅ Successfully sent notification");
+      }
+    } catch (error) {
+      console.error("❌ Error sending notification:", error.response?.data || error.message);
+    }
+  };
   return (
     <div>
       <h1>PWA Push Notifications (FCM)</h1>
       <p>FCM Token:</p>
       <textarea rows="5" cols="80" value={fcmToken} readOnly />
+      <button onClick={sendPushNotification}>Send Push Notification</button>
+
     </div>
   );
 }
