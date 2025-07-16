f3bd843c740cbd7aaa4b762885982417441e0d49
manoj <manoj@intern.schmidvision.com>
2025-07-16 14:59:22 +0530

    after adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
 import { useEffect, useState } from "react";
-import {registerForPush} from "./services/registerPush.js"
+import { registerForPush } from "./services/registerPush.js";
 import axios from "axios";
+
 function App() {
   const [token, setToken] = useState("");
-  const [platform,setPlatfrom]=useState("")
+  const [platform, setPlatform] = useState("");
 
   useEffect(() => {
-    registerForPush(({token,platform}) => {
+    registerForPush(({ token, platform }) => {
       setToken(token);
-      setPlatfrom(platform)
-
+      setPlatform(platform);
     });
   }, []);
-const sendTokenToBackend= async(token)=>{
-  try{
-    const reponse=   await axios.post("https://4f0eb7f39936.ngrok-free.app/notifcation",{
-      token
-    })
-    if(reponse.status==200){
-      alert("ur token sucessfully send to the  backend")
+
+  const sendTokenToBackend = async (token) => {
+    if (!token) {
+      alert("No token to send");
+      return;
     }
-  
-  }
-  catch(error){
-     alert(`errr0:${error.message}`)
+
+    try {
+      const response = await axios.post("https://4f0eb7f39936.ngrok-free.app/notification", {
+        token,
+      });
+      if (response.status === 200) {
+        alert("✅ Token successfully sent to backend");
+      }
+    } catch (error) {
+      alert(`❌ Error: ${error.message}`);
+    }
   };
- 
-
- 
-  
-}
-
 
   return (
     <div>
       <h1>PWA Push Notification</h1>
-      <p>FCM Token:</p>
+      <p>Push Token:</p>
       <textarea rows="5" cols="80" value={token} readOnly />
-      <textarea row="2" cols="20" value={platform} readOnly/>
-       <button  onClick={sendTokenToBackend}>sendTokenToBackend</button>
-
+      <p>Platform:</p>
+      <textarea rows="2" cols="20" value={platform} readOnly />
+      <br />
+      <button onClick={() => sendTokenToBackend(token)}>Send Token To Backend</button>
     </div>
   );
 }
